e0f5cd0b8decb785b29f039cd08f24ca2983ee44
Harry Torry <harrytorry@gmail.com>
2019-04-02 21:31:02 +0100

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>tell me the port</title>
+    <style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+  </head>
+  <body>
+  <table>
+  <tr>
+    <th>App</th>
+    <th>Port</th>
+  </tr>
+  <tr><td>Radarr</td><td>7878</td></tr>
+  <tr><td>Sonarr</td><td>8989</td></tr>
+  <tr><td>Plex</td><td>32400</td></tr>
+  </table>
+  </body>
+</html>
