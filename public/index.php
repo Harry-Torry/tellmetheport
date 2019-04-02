0495793e88a7336aab52c3954e8bca349e3f1e48
Harry Torry <harrytorry@gmail.com>
2019-04-02 21:39:12 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,43 +1,48 @@
 <?php
-?>
-
-
+   ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>tell me the port</title>
-    <style>
-      body {
-    width: 800px;
-    margin: 0 auto;
-      }
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
-
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-</style>
-  </head>
-  <body>
-  <table>
-  <tr>
-    <th>App</th>
-    <th>Port</th>
-  </tr>
-    <tr><td><a href="http://localhost:7878" target="_blank">Radarr</a></td><td>7878</td><td></tr>
-  <tr><td><a href="http://localhost:8989" target="_blank">Sonarr</a></td><td>8989</td></tr>
-    <tr><td><a href=http://127.0.0.1:32400/web/index.html" target="_blank">Plex</a></td><td>32400</td></tr>
-  </table>
-  </body>
+   <head>
+      <meta charset="UTF-8">
+      <title>tell me the port</title>
+      <style>
+         body {
+         width: 800px;
+         margin: 0 auto;
+         }
+         table {
+         font-family: arial, sans-serif;
+         border-collapse: collapse;
+         width: 100%;
+         }
+         td, th {
+         border: 1px solid #dddddd;
+         text-align: left;
+         padding: 8px;
+         }
+         tr:nth-child(even) {
+         background-color: #dddddd;
+         }
+      </style>
+   </head>
+   <body>
+      <table>
+         <tr>
+            <th>App</th>
+            <th>Port</th>
+         </tr>
+         <tr>
+            <td><a href="http://localhost:7878" target="_blank">Radarr</a></td>
+            <td>7878</td>
+         </tr>
+         <tr>
+            <td><a href="http://localhost:8989" target="_blank">Sonarr</a></td>
+            <td>8989</td>
+         </tr>
+         <tr>
+            <td><a href=http://127.0.0.1:32400/web/index.html" target="_blank">Plex</a></td>
+            <td>32400</td>
+         </tr>
+      </table>
+   </body>
 </html>
