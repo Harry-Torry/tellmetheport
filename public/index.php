ee40a06aefb4da7d08ff3a72f38a8c3ac3c63e54
Harry Torry <harrytorry@gmail.com>
2019-04-02 21:36:49 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,6 +8,10 @@
     <meta charset="UTF-8">
     <title>tell me the port</title>
     <style>
+      body {
+    width: 800px;
+    margin: 0 auto;
+      }
 table {
   font-family: arial, sans-serif;
   border-collapse: collapse;
@@ -31,9 +35,9 @@ tr:nth-child(even) {
     <th>App</th>
     <th>Port</th>
   </tr>
-  <tr><td>Radarr</td><td>7878</td></tr>
-  <tr><td>Sonarr</td><td>8989</td></tr>
-  <tr><td>Plex</td><td>32400</td></tr>
+    <tr><td><a href="http://localhost:7878" target="_blank">Radarr</a></td><td>7878</td><td></tr>
+  <tr><td><a href="http://localhost:8989" target="_blank">Sonarr</a></td><td>8989</td></tr>
+  <tr><a href=http://127.0.0.1:32400/web/index.html" target="_blank">Plex</a><td>32400</td></tr>
   </table>
   </body>
 </html>
